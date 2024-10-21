c6a72b52de77ddcfb71ac719baae08ed626ed7a7
Brad Reed <noisyscanner@users.noreply.github.com>
2024-10-21 14:20:16 +0100

    print log ine

--- metrics/src/logprocessor.rs
+++ metrics/src/logprocessor.rs
@@ -189,7 +189,7 @@ async fn handle_websocket_log(log: &BaseLog, metric_obj: &Telemetry) {
 }
 
 pub async fn log_processor(logline: &String, metric_obj: &Telemetry, tracer: &trace::Tracer) {
-    //println!("{}", logline);
+    println!("{}", logline);
     metric_obj.LOG_LINES_COUNTER_TOTAL.inc();
     let log_result = from_str::<BaseLog>(logline);
     match log_result {
